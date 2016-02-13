1f616511b1bb0ab8ca8e60fd8cdd83573d96245d
Wim Rademaker <wimrademaker@gmail.com>
2016-02-13 23:22:57 +0100

    js script bugfix and removing dubug code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-$(window).load(function){
+$(window).load(function(){
    $('.pageblock').css('min-height', $(window).height()+'px');
 })
 
 $(document).ready( function() {
    $(".next-block").click(function() {
-     console.log('click');
      var i = $(this).closest(".pageblock").index()+1;
-     console.log(i)
      $('html, body').animate({
        scrollTop: $(".pageblock:nth-child("+(i+1)+")").offset().top
      }, 600);
