c8d6638029ad1bc386e1cced8f200958faad0234
Wim Rademaker <wimrademaker@gmail.com>
2016-02-13 22:49:53 +0100

    eerste info erop gezet

---
+++ js/script.js
@@ -0,0 +1,22 @@
+ $(document).ready( function() {
+
+   $('.pageblock').css('min-height', $(window).height()+'px');
+
+
+
+
+   $(".next-block").click(function() {
+     console.log('click');
+     var i = $(this).closest(".pageblock").index()+1;
+     console.log(i)
+     $('html, body').animate({
+       scrollTop: $(".pageblock:nth-child("+(i+1)+")").offset().top
+     }, 600);
+   });
+
+ })
+
+
+ $(window).on('resize', function(){
+    $('.pageblock').css('height', $(window).height()+'px');
+ })
